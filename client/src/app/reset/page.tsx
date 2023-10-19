dc3395b3593e31e721ebab6835b107dcc723d9e5
Mickael Brunet <mickael.brunet64@gmail.com>
2023-10-19 14:06:10 +0200

    modify ci

--- client/src/app/reset/page.tsx
+++ client/src/app/reset/page.tsx
@@ -5,7 +5,7 @@ const ResetPassword = () => {
     <div>
       <div>
         <input type="text" name="input" />
-        Change this file rechange this file DFBKkjijb
+        Change this file rechange this file DFBKkjijbded
       </div>
     </div>
   )
