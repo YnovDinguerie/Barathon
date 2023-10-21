bbed9a8ae947732a72cecad4cb8ba70e98ef6131
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2023-10-21 20:05:22 +0200

    fix: search bar and new ui

--- client/src/components/Bottom.tsx
+++ client/src/components/Bottom.tsx
@@ -2,18 +2,31 @@ import React, { useState } from 'react'
 import './Bottom.scss'
 import './Filter.scss'
 import RangeInput from './Input/RangeInput'
+import { useAtom } from 'jotai'
+import { barsToVisitAtom, radiusAtom } from './atoms'
 
 const Bottom = () => {
   const [setupBarathon, setSetupBarathon] = useState(false)
-  const [barsToVisit, setBarsToVisit] = useState(5)
-  const [radiusBars, setRadiusBars] = useState(3)
+  const [startBarathon, setStartBarathon] = useState(false)
+  const [barsToVisit, setBarsToVisit] = useAtom(barsToVisitAtom)
+  const [radiusBars, setRadiusBars] = useAtom(radiusAtom)
 
-  const startBarathon = () => {
+  const setupBarathonFunction = () => {
     setSetupBarathon(true)
+  }
+
+  const startBarathonFunction = () => {
+    setSetupBarathon(false)
+    setStartBarathon(true)
   }
 
   const backToMenu = () => {
     setSetupBarathon(false)
+  }
+
+  const stopGame = () => {
+    setSetupBarathon(false)
+    setStartBarathon(false)
   }
 
   const handleRangeBarNumberChange = (barsToVisit: number) => {
@@ -24,97 +37,118 @@ const Bottom = () => {
     setRadiusBars(barsRadius)
   }
 
-  const startSearchBarathon = () => {
-    console.log(barsToVisit)
-    console.log(radiusBars)
+  let content
+
+  if (setupBarathon) {
+    content = (
+      <div>
+        <h1 className="title">Démarrer le Barathon</h1>
+        <h2 className="section">Nombre de bars à visiter</h2>
+        <div className="container-slider">
+          <RangeInput
+            min={2}
+            max={10}
+            onRangeChange={handleRangeBarNumberChange}
+          />
+          <p className="number-information">{barsToVisit} bars </p>
+        </div>
+        <h2 className="section">Rayon du Barathon (en km)</h2>
+        <div className="container-slider">
+          <RangeInput
+            min={2}
+            max={10}
+            onRangeChange={handleRangeRadiusChange}
+          />
+          <p className="number-information">{radiusBars} km </p>
+        </div>
+        <h2 className="section">Paramètres avancés</h2>
+        <button
+          onClick={startBarathonFunction}
+          className="bg-orange text-white start-barathon"
+        >
+          Démarrer la partie
+        </button>
+
+        <div className="container-back-menu">
+          <img
+            className="back-menu"
+            src="assets/arrow-left.svg"
+            alt="back to menu"
+          />
+          <p onClick={backToMenu} className="text-back-menu">
+            Revenir en arrière
+          </p>
+        </div>
+      </div>
+    )
+  } else if (startBarathon) {
+    content = (
+      <div>
+        <div className="section-container">
+          <div>
+            <p className="section-title">Temps total</p>
+            <p className="total-time"> 2h30 </p>
+          </div>
+          <div onClick={stopGame} className="container-button-game">
+            <img className="stop-image" src="assets/stop.svg" alt="stop" />
+            <p className="count-element">Arrêter</p>
+          </div>
+          <div>
+            <p className="section-title">Bars restant</p>
+            <div className="container-bar-left">
+              <img src="assets/beer2.svg" alt="bars left" />
+              <p className="count-bar-left"> 2 </p>
+            </div>
+          </div>
+        </div>
+        <div className="loading-bar"></div>
+      </div>
+    )
+  } else {
+    content = (
+      <div>
+        <div className="filter-container">
+          <img
+            className="search-icon"
+            src="assets/search.svg"
+            alt="search icon"
+          />
+          <input type="text" placeholder="Où va t-on ?" />
+          <img
+            className="microphone-icon"
+            src="assets/microphone.svg"
+            alt="Microphone"
+          />
+        </div>
+        <h2 className="section"> Favories </h2>
+        <div className="section-container">
+          <img className="beer-icon" src="assets/beer.svg" alt="beer icon" />
+          <div className="localisation-container">
+            <h3 className="bar-name"> La Cervoiserie </h3>
+            <p> 17 Pl. du Palais, 33000 Bordeaux </p>
+          </div>
+          <div>10 mins</div>
+        </div>
+        <h2 className="section"> Récents </h2>
+        <div className="section-container">
+          <img className="beer-icon" src="assets/beer.svg" alt="beer icon" />
+          <div className="localisation-container">
+            <h3 className="bar-name"> La Cervoiserie </h3>
+            <p> 17 Pl. du Palais, 33000 Bordeaux </p>
+          </div>
+          <div>10 mins</div>
+        </div>
+        <button
+          onClick={setupBarathonFunction}
+          className="bg-orange text-white start-barathon"
+        >
+          Démarrer un barathon
+        </button>
+      </div>
+    )
   }
 
-  return (
-    <div className="container-bottom bg-white">
-      {setupBarathon ? (
-        <div>
-          <h1 className="title">Démarrer le Barathon</h1>
-          <h2 className="section">Nombre de bars à visiter</h2>
-          <div className="container-slider">
-            <RangeInput
-              min={2}
-              max={10}
-              onRangeChange={handleRangeBarNumberChange}
-            />
-            <p className="number-information">{barsToVisit} bars </p>
-          </div>
-          <h2 className="section">Rayon du Barathon (en km)</h2>
-          <div className="container-slider">
-            <RangeInput
-              min={2}
-              max={10}
-              onRangeChange={handleRangeRadiusChange}
-            />
-            <p className="number-information">{radiusBars} km </p>
-          </div>
-          <h2 className="section">Paramètres avancés</h2>
-          <button
-            onClick={startSearchBarathon}
-            className="bg-orange text-white start-barathon"
-          >
-            Démarrer la recherche
-          </button>
-
-          <div className="container-back-menu">
-            <img
-              className="back-menu"
-              src="assets/arrow-left.svg"
-              alt="back to menu"
-            />
-            <p onClick={backToMenu} className="text-back-menu">
-              Revenir en arrière
-            </p>
-          </div>
-        </div>
-      ) : (
-        <div>
-          {' '}
-          <div className="filter-container">
-            <img
-              className="search-icon"
-              src="assets/search.svg"
-              alt="search icon"
-            />
-            <input type="text" placeholder="Où va t-on ?" />
-            <img
-              className="microphone-icon"
-              src="assets/microphone.svg"
-              alt="Microphone"
-            />
-          </div>
-          <h2 className="section"> Favories </h2>
-          <div className="section-container">
-            <img className="beer-icon" src="assets/beer.svg" alt="beer icon" />
-            <div className="localisation-container">
-              <h3 className="bar-name"> La Cervoiserie </h3>
-              <p> 17 Pl. du Palais, 33000 Bordeaux </p>
-            </div>
-            <div>10 mins</div>
-          </div>
-          <h2 className="section"> Récents </h2>
-          <div className="section-container">
-            <img className="beer-icon" src="assets/beer.svg" alt="beer icon" />
-            <div className="localisation-container">
-              <h3 className="bar-name"> La Cervoiserie </h3>
-              <p> 17 Pl. du Palais, 33000 Bordeaux </p>
-            </div>
-            <div>10 mins</div>
-          </div>
-          <button
-            onClick={startBarathon}
-            className="bg-orange text-white start-barathon"
-          >
-            Démarrer un barathon
-          </button>
-        </div>
-      )}
-    </div>
-  )
+  return <div className="container-bottom bg-white">{content}</div>
 }
 
 export default Bottom
--- client/src/components/MapboxMap.tsx
+++ client/src/components/MapboxMap.tsx
@@ -1,45 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import mapboxgl, { Marker } from 'mapbox-gl'
 import { useAtom } from 'jotai'
-import { latitudeAtom, longitudeAtom } from './atoms'
+import {
+  barsToVisitAtom,
+  latitudeAtom,
+  longitudeAtom,
+  radiusAtom,
+} from './atoms'
 import './MapboxMap.scss'
 import axios from 'axios'
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiZXhvc2xhc2gzMyIsImEiOiJjbG53dTkzcGgwOXRnMmpsZWplcXIxeGt1In0.vE7rIXLdqq640qg8Otz2Pw'
 
-export const getBars = async (
-  latitude: number | string,
-  longitude: number | string,
-  radiusBars: number | string,
-) => {
-  try {
-    const res = await axios.get(
-      `http://127.0.0.1:8000/api/bars/${latitude}&${longitude}&${radiusBars}`,
-    )
-
-    const datas = res.data.data
-    for (const data of datas) {
-      createCustomMarker([data.longitude, data.latitude], './assets/beer.svg')
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error)
-  }
-}
-
-const createCustomMarker = (lngLat: any, imagePath: any) => {
-  const markerElement = document.createElement('div')
-  markerElement.className = 'custom-marker'
-  const markerImg = document.createElement('img')
-  markerImg.src = imagePath
-  markerElement.appendChild(markerImg)
-
-  return new Marker(markerElement).setLngLat(lngLat).addTo(map)
-}
-
 const MapboxMap = () => {
   const [latitude] = useAtom(latitudeAtom)
   const [longitude] = useAtom(longitudeAtom)
+  const [radius] = useAtom(radiusAtom)
+  const [barsToVisit] = useAtom(barsToVisitAtom)
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -59,10 +39,79 @@ const MapboxMap = () => {
 
     map.addControl(geolocateControl)
 
-    return () => {
-      map.remove()
+    // Fonction pour calculer la distance entre deux points
+    function calculateDistance(
+      lat1: number,
+      lon1: number,
+      lat2: number,
+      lon2: number,
+    ) {
+      const R = 6371 // Rayon de la Terre en kilomètres
+      const dLat = (lat2 - lat1) * (Math.PI / 180)
+      const dLon = (lon2 - lon1) * (Math.PI / 180)
+      const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * (Math.PI / 180)) *
+          Math.cos(lat2 * (Math.PI / 180)) *
+          Math.sin(dLon / 2) *
+          Math.sin(dLon / 2)
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+      const distance = R * c // Distance en kilomètres
+      return distance
     }
-  }, [latitude, longitude])
+
+    const getBars = async () => {
+      try {
+        const res = await axios.get(
+          `http://127.0.0.1:8000/api/bars/${latitude}&${longitude}&${radius}`,
+        )
+
+        const datas = res.data.data
+
+        // Calculer la distance pour chaque bar
+        const barsWithDistance = datas.map(
+          (bar: { latitude: string; longitude: string }) => ({
+            ...bar,
+            distance: calculateDistance(
+              latitude,
+              longitude,
+              parseFloat(bar.latitude),
+              parseFloat(bar.longitude),
+            ),
+          }),
+        )
+
+        // Trier les bars par distance croissante
+        const sortedBars = barsWithDistance.sort(
+          (a: { distance: number }, b: { distance: number }) =>
+            a.distance - b.distance,
+        )
+
+        // Garder seulement les 3 premiers éléments
+        const barsSliced = sortedBars.slice(0, barsToVisit)
+
+        console.log(barsSliced)
+
+        for (const bar of barsSliced) {
+          createCustomMarker([bar.longitude, bar.latitude], './assets/beer.svg')
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    const createCustomMarker = (lngLat: any, imagePath: any) => {
+      const markerElement = document.createElement('div')
+      markerElement.className = 'custom-marker'
+      const markerImg = document.createElement('img')
+      markerImg.src = imagePath
+      markerElement.appendChild(markerImg)
+
+      return new Marker(markerElement).setLngLat(lngLat).addTo(map)
+    }
+
+    getBars()
+  }, [latitude, longitude, radius, barsToVisit])
 
   return <div id="map-container" className="map-container"></div>
 }
