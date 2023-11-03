a68d98eea2fe45c52c373a2d6422cf66a250e4b0
YohannTonnerre <yohann.tonnerre@gmail.com>
2023-11-03 14:29:15 +0100

    lint

--- server/app/Http/Controllers/API/SocketTestController.php
+++ server/app/Http/Controllers/API/SocketTestController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers\API;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\API\BaseController as BaseController;
-use Validator;
-
 use App\Events\TestEvent;
+use App\Http\Controllers\API\BaseController as BaseController;
+
 class SocketTestController extends BaseController
 {
-    public function index(){
+    public function index()
+    {
         broadcast(new TestEvent());
     }
 }
