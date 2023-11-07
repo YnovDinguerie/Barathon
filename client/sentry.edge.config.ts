72584e4f0932b335eb7425919b0b6e81a4e9f74f
YohannTonnerre <yohann.tonnerre@gmail.com>
2023-11-07 19:50:15 +0100

    :sparkles: update sentry config

---
+++ client/sentry.edge.config.ts
@@ -0,0 +1,16 @@
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+    dsn: 'https://80e2c68e14cba2bceb53a94315877a98@o4506186000498688.ingest.sentry.io/4506186003382272',
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+
+    // ...
+
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+})
