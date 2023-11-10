f1a509d485324cef6cb74f561f5612d2fe1d6af2
Mickael Brunet <mickael.brunet64@gmail.com>
2023-11-10 14:23:51 +0100

    getBaratons on stat page

--- client/src/app/profile/stats/page.tsx
+++ client/src/app/profile/stats/page.tsx
@@ -1,8 +1,25 @@
 'use client'
 
+import getBarathons from '@/app/api/barathon/getBarathons'
 import Statistics from '@/components/stats/Statistics'
+import { toastAtom, userAtom } from '@/state'
+import { useQuery } from '@tanstack/react-query'
+import { useAtomValue, useSetAtom } from 'jotai'
 
 const Stats = () => {
+  const { token } = useAtomValue(userAtom)
+  const setToast = useSetAtom(toastAtom)
+  const { data: barathons } = useQuery({
+    queryKey: ['barathons'],
+    queryFn: () =>
+      getBarathons({ userToken: token }).catch((error) => {
+        setToast({
+          msg: error.response.data.message,
+          status: 'Error',
+          isVisible: true,
+        })
+      }),
+  })
   const data = [
     { name: '01/11/2023', value: 5 },
     { name: '05/11/2023', value: 10 },
