5faf4ed219d7260cf946fe353cf592eaa853586c
YohannTonnerre <yohann.tonnerre@gmail.com>
2023-11-16 10:54:56 +0100

    :sparkles: fix: fix sentry

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,7 +3,12 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 import ToastComponent from '@/components/toast/ToastComponent'
+import { init } from '@sentry/nextjs'
 
+init({
+  dsn: 'https://80e2c68e14cba2bceb53a94315877a98@o4506186000498688.ingest.sentry.io/4506186003382272',
+  // Ajoutez d'autres options de configuration de Sentry si nécessaire
+})
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
